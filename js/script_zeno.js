ee07d22a10a9ede9203e42fb2b19dfefe3601690
Bintang Tenggara <51829646+bintangtenggara@users.noreply.github.com>
2026-02-25 05:00:53 +0700

    Update script_zeno.js

--- js/script_zeno.js
+++ js/script_zeno.js
@@ -28,7 +28,7 @@ const url = 'https://api.zeno.fm/mounts/metadata/subscribe/kc67gypcxghtv';
 const API_KEY = "18fe07917957c289983464588aabddfb";
 
 // DEFAULT COVER
-const DEFAULT_COVER_ART = 'https://cdn4.mbahnunungonline.net/img/NoCover.png';
+const DEFAULT_COVER_ART = 'https://cdn-profiles.tunein.com/s162395/images/logod.png?t=638531136760000000';
 
 // Variable to control history display: true = display / false = hides
 let showHistory = true; 
